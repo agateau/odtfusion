1eb3c29d69ad5598d8e1ccc52ad8b60c70a47fea
Aurélien Gâteau <agateau@kde.org>
2011-06-24 22:10:12 +0200

    New helper tool to dump the xml of a .odt

---
+++ odtdump.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+from optparse import OptionParser
+
+from odtfile import OdtFile
+
+USAGE = "%prog <odt-file-name>"
+DESCRIPTION = "Dumps an indented version of the content.xml of a .odt file to stdout."
+
+def main():
+    parser = OptionParser(usage=USAGE, description=DESCRIPTION)
+    (options, args) = parser.parse_args()
+    if len(args) != 1:
+        parser.error("Missing arguments")
+
+    odt_name = args[0]
+    odt = OdtFile(odt_name)
+    odt.tree.write(sys.stdout, pretty_print=True)
+
+    return 0
+
+
+if __name__=="__main__":
+    sys.exit(main())
+# vi: ts=4 sw=4 et
