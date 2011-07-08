8d47f6748b631c229ddfc992668471820ee26b52
Aurélien Gâteau <agateau@kde.org>
2011-07-08 12:28:11 +0200

    unicode-ify

--- tests/unit/textfilecachetestcase.py
+++ tests/unit/textfilecachetestcase.py
@@ -14,7 +14,7 @@ from StringIO import StringIO
 
 from textfilecache import _split_file, TextFileCache
 
-HELLO = """
+HELLO = u"""
 /// 1
 Hello
 
@@ -23,10 +23,14 @@ World
 """
 
 class TextFileCacheTestCase(unittest.TestCase):
+    def _write_test_file(self, name, content):
+        full_path = os.path.join(self.sb_dir, name)
+        content_utf8 = content.encode("utf-8")
+        open(full_path, "w").write(content_utf8)
+
     def setUp(self):
         self.sb_dir = tempfile.mkdtemp(prefix="sandbox-", dir=".")
-        full_path = os.path.join(self.sb_dir, "hello")
-        open(full_path, "w").write(HELLO)
+        self._write_test_file("hello", HELLO)
 
     def tearDown(self):
         shutil.rmtree(self.sb_dir)
@@ -35,8 +39,8 @@ class TextFileCacheTestCase(unittest.TestCase):
         fl = StringIO(HELLO)
         dct = _split_file(fl)
         self.assertEqual(len(dct), 2)
-        self.assertEqual(dct["1"], "Hello\n")
-        self.assertEqual(dct["2"], "World\n")
+        self.assertEqual(dct["1"], u"Hello\n")
+        self.assertEqual(dct["2"], u"World\n")
 
     def test_has_key(self):
         cache = TextFileCache(self.sb_dir)
