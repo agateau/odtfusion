71b7398211150015c727d9ade7fa7578c50a7ce2
Robert Malvern <rob@randomtask.co.uk>
2013-08-27 09:47:27 +0100

    File permission problems on windows (review)
    Review comments included on modification to fix file permission errors
    in zipfile lib on windows.

--- odtfile.py
+++ odtfile.py
@@ -5,8 +5,8 @@ This file is part of the odtfusion project.
 @author: Aurélien Gâteau <aurelien.gateau@free.fr>
 @license: GPL v3 or later
 """
-import shutil
 import os
+import shutil
 from tempfile import NamedTemporaryFile
 from zipfile import ZipFile
 
@@ -24,11 +24,16 @@ class OdtFile(object):
     def save(self, name):
         assert self._input_name != name
         shutil.copy(self._input_name, name)
-        with NamedTemporaryFile(delete=False) as tempFile:
-            self.tree.write(tempFile, encoding="utf-8")
-            tempFile.flush()
-            with ZipFile(name, "a") as zip:
-                zip.write(tempFile.name, "content.xml")
+        try:
+            with NamedTemporaryFile(delete=False) as tempFile:
+            # delete=False is used to prevent permission denied error 
+            # from zipfile lib when trying to open temporary file on windows
+                self.tree.write(tempFile, encoding="utf-8")
+                tempFile.flush()
+                with ZipFile(name, "a") as zip:
+                    zip.write(tempFile.name, "content.xml")
+        finally:
             tempFile.close()
+            # manual close required else remove blocked below.
             os.remove(tempFile.name)
 # vi: ts=4 sw=4 et
